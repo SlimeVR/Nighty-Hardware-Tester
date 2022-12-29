4b5409173def497e6b48d57708e53b31abfa6589
DevMiner <devminer@devminer.xyz>
2022-12-29 20:21:20 +0100

    fix(web): double scrollbars

--- backend/src/pages/_app.tsx
+++ backend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { trpc } from "../utils/trpc";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className="min-h-full bg-bg p-2 text-white overflow-y-scroll">
+    <div className="h-full bg-bg p-2 text-white overflow-y-scroll">
       <Component {...pageProps} />
     </div>
   );
